8ec73be252209f75ca85d20857fbe77cdd13b776
ekcaiki <tmp10dkbe@hotmail.com>
2023-10-18 20:46:16 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Put down your name
+my_name = "Daryl"
+# Put down the course name you're working on
+course_name = "Python"
+# Current month (in integer please!)
+curr_month = 9  # change this to ..?
+
+print(
+    f"Hello {my_name}! The course ends on Dec, which is {12-curr_month} months from now."
+)
+print("Hope you have a great time learing this course")
+
+today = 86.7
+yesterday = 87.0
+# print out the average temperuatre of today and yesterday
+avg_temp = (today + yesterday) / 2  # fill in here!
+print(f"average temperature is {avg_temp}")
